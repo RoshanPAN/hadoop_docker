0820dce33053613dcc704e1484311d3c18fe71e8
Luoshang Pan <panluoshang@live.com>
2017-09-20 15:44:39 -0400

    Edit fs.defaultFS -> port, add helping command into prepare_host.sh

--- prepare_host.sh
+++ prepare_host.sh
@@ -9,3 +9,10 @@ ufw allow 8030 && ufw allow 8031 && ufw allow 8033 && ufw allow 8040 \
   && ufw allow 8042 && ufw allow 8088 
 # 2122 = sshd, 
 ufw allow 49707 && ufw allow 2122
+
+
+# Build Docker Image
+docker build -t="pls331/centos:psuedo-distributed" .
+
+# Create Contrainer from Image
+docker run -it -p 50010:50010 -p 2122:2122 pls331/centos:psuedo-distributed /etc/bootstrap.sh -bash
