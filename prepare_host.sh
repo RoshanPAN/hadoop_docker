7dca30803e037060abb584b5990edd258b49ef7e
Luoshang Pan <panluoshang@live.com>
2017-09-20 22:36:53 -0400

    tmp

--- prepare_host.sh
+++ prepare_host.sh
@@ -1,6 +1,6 @@
 # Allow ports in firewall
 # Hdfs ports
-ufw allow 50010 && ufw allow 50020 && ufw allow 50075 && ufw allow 50090 \
+ufw allow 50010 && ufw allow 50020 && ufw allow 50075 && ufw allow 50070 && ufw allow 50090 \
   && ufw allow 8020 && ufw allow 9000 
 # Mapred ports
 ufw allow 10020 && ufw allow 19888
@@ -15,4 +15,4 @@ ufw allow 49707 && ufw allow 2122
 docker build -t="pls331/centos:psuedo-distributed" .
 
 # Create Contrainer from Image
-docker run -it -p 50010:50010 -p 2122:2122 pls331/centos:psuedo-distributed /etc/bootstrap.sh -bash
+docker run -it -p 50010:50010 -p 2122:2122 -p 50070:50070 pls331/centos:psuedo-distributed /etc/bootstrap.sh -bash
