c3f1b1cf5f64d8c16c71e3179999131f765347b6
Luoshang Pan <panluoshang@live.com>
2017-11-09 10:16:08 -0500

    fix previous bug

--- restart-hdfs.sh
+++ restart-hdfs.sh
@@ -4,8 +4,8 @@
 # 2. Format the namenode
 # 3. start cluster using my-start-cluster-from-master.sh 
 $HADOOP_PREFIX/sbin/stop-all.sh && sleep 10 && \
-       ssh machine01 "rm -r $HADOOP_PREFIX/tmp/dfs/data/* && \
-       ssh machine02 "rm -r $HADOOP_PREFIX/tmp/dfs/data/* && \
-       ssh machine03 "rm -r $HADOOP_PREFIX/tmp/dfs/data/* && \
+       ssh machine01 "rm -r $HADOOP_PREFIX/tmp/dfs/data/*" && \
+       ssh machine02 "rm -r $HADOOP_PREFIX/tmp/dfs/data/*" && \
+       ssh machine03 "rm -r $HADOOP_PREFIX/tmp/dfs/data/*" && \
        $HADOOP_PREFIX/bin/hadoop namenode -format && \
        $HADOOP_PREFIX/my-start-cluster-from-master.sh
