81168cb673f2c8ee2eefa627fd0cd9789e4f47d2
Luoshang Pan <panluoshang@live.com>
2017-11-09 10:12:31 -0500

    3 replication cluster

--- restart-hdfs.sh
+++ restart-hdfs.sh
@@ -4,6 +4,7 @@
 # 2. Format the namenode
 # 3. start cluster using my-start-cluster-from-master.sh 
 $HADOOP_PREFIX/sbin/stop-all.sh && sleep 10 && \
+       ssh machine01 "rm -r $HADOOP_PREFIX/tmp/dfs/data/* && \
        ssh machine02 "rm -r $HADOOP_PREFIX/tmp/dfs/data/* && \
        ssh machine03 "rm -r $HADOOP_PREFIX/tmp/dfs/data/* && \
        $HADOOP_PREFIX/bin/hadoop namenode -format && \
